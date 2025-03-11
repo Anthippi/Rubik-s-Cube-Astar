b03342eb8e17a5b797de7297c5f221dc7240e2f5
Anthippi <59541666+Anthippi@users.noreply.github.com>
2025-03-11 22:03:49 +0200

    Update Astar.py

--- Astar.py
+++ Astar.py
@@ -1,20 +1,30 @@
-import heapq
+from queue import PriorityQueue
+from collections import defaultdict
 
 class AStarSolver:
     def __init__(self, initial_state):
-        self.open = []
-        self.closed = set()
-        heapq.heappush(self.open, (initial_state.f, id(initial_state), initial_state))
+        self.open = PriorityQueue()
+        self.closed = defaultdict(bool)
+        self.open.put((initial_state.f, initial_state))
+        self.came_from = {}  # Για να παρακολουθείτε το μονοπάτι
 
     def solve(self):
-        while self.open:
-            current = heapq.heappop(self.open)[2]
+        while not self.open.empty():
+            current = self.open.get()[1]
             if current.is_final():
-                return current.get_path()
-            if current in self.closed:
+                return self.reconstruct_path(current)
+            if self.closed[hash(current)]:
                 continue
-            self.closed.add(current)
+            self.closed[hash(current)] = True
             for child in current.get_children():
-                if child not in self.closed:
-                    heapq.heappush(self.open, (child.f, id(child), child))
+                if not self.closed.get(hash(child), False):
+                    self.open.put((child.f, child))
+                    self.came_from[child] = current
         return None
+
+    def reconstruct_path(self, current):
+        path = []
+        while current in self.came_from:
+            path.append(current.move)
+            current = self.came_from[current]
+        return path[::-1]
